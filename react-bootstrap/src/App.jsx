82925ee8e7311c29efff661f1cd243cb357eddb8
Diego01-21 <diego.calapaqui@iti.edu.ec>
2026-05-24 14:49:45 -0500

    Proyecto React

--- react-bootstrap/src/App.jsx
+++ react-bootstrap/src/App.jsx
@@ -5,60 +5,43 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  // ARRAY DONDE SE GUARDAN LOS PRODUCTOS
+
+  // Array para guardar productos
   const [productos, setProductos] = useState([]);
-  // OBJETO PARA MANEJAR EL FORMULARIO
+  // Objeto prar manejar formulario
   const [producto, setProducto] = useState({
+    
     id: "",
     nombre: "",
     descripcion: "",
     compra: "",
     venta: "",
   });
-
-  // CAPTURA LOS DATOS DE LOS INPUTS
+  // entrada de datos
   const handleChange = (e) => {
+    setProducto({...producto, [e.target.name]: e.target.value,});
 
-    setProducto({
-      ...producto,
-      [e.target.name]: e.target.value,
-    });
   };
-
-  // FUNCIÓN PARA GUARDAR PRODUCTOS
-  const guardarProducto = (e) => {
-
-    // EVITA QUE LA PÁGINA SE RECARGUE
-    e.preventDefault();
-
-    // VALIDAR CAMPOS VACÍOS
+  // guardar productos
+  const guardarProducto = (e) => { e.preventDefault();
+    // Validar campos vacios
     if (
-      producto.id === "" ||
-      producto.nombre === "" ||
-      producto.descripcion === "" ||
-      producto.compra === "" ||
-      producto.venta === ""
+      producto.id === "" || producto.nombre === "" || producto.descripcion === "" || producto.compra === "" || producto.venta === ""
     ) {
-
       alert("Todos los campos son obligatorios");
       return;
     }
+    // Validar precio venta > compra
 
-    // VALIDAR QUE EL PRECIO DE VENTA
-    // SEA MAYOR AL DE COMPRA
     if (parseFloat(producto.venta) <= parseFloat(producto.compra)) {
-
       alert("El precio de venta debe ser mayor al precio de compra");
       return;
     }
-
-    // AGREGAR PRODUCTO AL ARRAY
+    // Agregar productos a la tablas
     setProductos([...productos, producto]);
 
-    // MENSAJE DE ÉXITO
     alert("Producto guardado correctamente");
-
-    // LIMPIAR FORMULARIO
+    // Limpiar formulario
     setProducto({
       id: "",
       nombre: "",
@@ -67,190 +50,117 @@ function App() {
       venta: "",
     });
   };
-
-  // FUNCIÓN PARA ELIMINAR PRODUCTOS
+  // Eliminar productos
   const eliminarProducto = (id) => {
+    // mensaje de confirmacion
+    const confirmar = window.confirm("Seguro que desea eliminar el producto ");
 
-    // VENTANA DE CONFIRMACIÓN
-    const confirmar = window.confirm(
-      "¿Seguro que desea eliminar el producto?"
-    );
-
-    // SI EL USUARIO ACEPTA
     if (confirmar) {
+      // 
+      const nuevaLista = productos.filter( (item) => item.id !== id);
 
-      // FILTRAR PRODUCTOS DIFERENTES AL ID
-      const nuevaLista = productos.filter(
-        (item) => item.id !== id
-      );
-
-      // ACTUALIZAR ARRAY
       setProductos(nuevaLista);
     }
   };
-  // RETORNA TODO EL HTML
+
+  //  HtML
+
   return (
     <div className="container mt-5 mb-5">
 
-      {/* CARD DEL FORMULARIO */}
+      {/* Contenedor para el formulario */}
       <div className="card shadow p-4 formulario">
-
         <h2 className="titulo mb-4">
-           Productos
+          Productos
         </h2>
-
+        /* FORMULARIO */
         <form onSubmit={guardarProducto}>
-
           <div className="row">
-
-            {/* INPUT ID */}
+            {/* Entrada iD*/}
             <div className="col-md-6 mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="ID Producto"
-                name="id"
-                value={producto.id}
+              <input type="text" className="form-control" placeholder="ID producto" name="id" value={producto.id}
                 onChange={handleChange}
               />
             </div>
-
-            {/* INPUT NOMBRE */}
+            {/* Entrada nombre */}
             <div className="col-md-6 mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nombre Producto"
-                name="nombre"
-                value={producto.nombre}
+              <input type="text" className="form-control" placeholder="Nombre producto" name="nombre" value={producto.nombre}
                 onChange={handleChange}
               />
             </div>
-
           </div>
-
-          {/* TEXTAREA DESCRIPCIÓN */}
+          {/* eentrada texto descripcion */}
           <div className="mb-3">
-            <textarea
-              className="form-control"
-              placeholder="Descripción del Producto"
-              name="descripcion"
-              rows="3"
+            <textarea className="form-control" placeholder="Descripcion del producto" name="descripcion" rows="3"
               value={producto.descripcion}
               onChange={handleChange}
             />
           </div>
-
           <div className="row">
-
-            {/* INPUT PRECIO COMPRA */}
+            {/* Entrada precio compra */}
             <div className="col-md-6 mb-3">
-              <input
-                type="number"
-                step="0.01"
-                className="form-control"
-                placeholder="Precio Compra"
-                name="compra"
-                value={producto.compra}
+              <input type="number" className="form-control" placeholder="Precio compra" name="compra" value={producto.compra}
                 onChange={handleChange}
               />
             </div>
-
-            {/* INPUT PRECIO VENTA */}
+            {/* Enrtada  precio venta */}
             <div className="col-md-6 mb-3">
-              <input
-                type="number"
-                step="0.01"
-                className="form-control"
-                placeholder="Precio Venta"
-                name="venta"
-                value={producto.venta}
+              <input type="number" className="form-control" placeholder="Precio venta" name="venta" value={producto.venta}
                 onChange={handleChange}
               />
             </div>
-
           </div>
-
-          {/* BOTÓN GUARDAR */}
-          <button type="submit" className="btn btn-success w-100 boton-guardar">
-            Guardar Producto
+          {/* Btn guardar */}
+          
+          <button type="submit" className="btn btn-success w-100 boton-guardar"><i className="bi bi-save me-2"></i>
+            Guardar
           </button>
-
         </form>
-
       </div>
-
-      {/* TABLA DE PRODUCTOS */}
+      {/* Tabla de productos */}
       <div className="card shadow p-4 mt-5 tabla-productos">
-
         <h3 className="mb-4 text-center">
           Lista de Productos
         </h3>
-
         {
           productos.length === 0 ? (
-
-            // MENSAJE SI NO HAY PRODUCTOS
-            <div className="alert alert-warning text-center">
-              No existen productos registrados
-            </div>
-
-            ) : (
-
+            <div className="alert alert-warning text-center">No hay
+             productos registrados</div>) : (
             <div className="table-responsive">
-
               <table className="table table-hover table-bordered align-middle">
-
-                <thead className="table-dark">
+                <thead className="table-primary">
                   <tr>
-                    <th>ID</th>
+                    <th>iD</th>
                     <th>Nombre</th>
-                    <th>Descripción</th>
+                    <th>Descripcion</th>
                     <th>Compra</th>
                     <th>Venta</th>
                     <th>Acciones</th>
                   </tr>
                 </thead>
-
                 <tbody>
-
                   {
                     productos.map((item) => (
-
                       <tr key={item.id}>
-
                         <td>{item.id}</td>
                         <td>{item.nombre}</td>
                         <td>{item.descripcion}</td>
                         <td>${item.compra}</td>
                         <td>${item.venta}</td>
-
                         <td>
-
-                          {/* BOTÓN ELIMINAR */}
-                          <button
-                            className="btn btn-danger btn-sm"
-                            onClick={() => eliminarProducto(item.id)}
-                          >
-                            Eliminar
-                          </button>
-
+                          {/* Boton eliminar */}
+                          <button className="btn btn-danger btn-sm" onClick={() => eliminarProducto(item.id)}>
+                            Eliminar</button>
                         </td>
-
                       </tr>
                     ))
                   }
-
                 </tbody>
-
               </table>
-
             </div>
           )
         }
-
       </div>
-
     </div>
   );
 }
